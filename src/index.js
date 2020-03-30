5fdbf065029b909c4fd369a343d43bc766d0b131
Olusola Olagoroye <solaolagoroye@gmail.com>
2020-03-30 10:07:43 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+// document.addEventListener("DOMContentLoaded", () => {
+//   // your code here
+// });
+
+
+const main = document.querySelector("#main-content")
+const taskForm = document.querySelector("#create-task-form")
+const taskList = document.querySelector("#task")
+
+taskForm.addEventListener("submit", function(e){
+  e.preventDafault()
+  // user input
+  const newTask = document.querySelector("#new-task-description").value
+
+const taskItem = document.createElement("li")
+taskItem.innerText = "table"
+
+taskList.appendChild(taskItem)
+})
